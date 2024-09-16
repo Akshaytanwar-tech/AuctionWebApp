d6117a8bc1b9cb875a5cf173f3784ad18b0bc1fa
akshay <akshaytanwar1701@gmail.com>
2024-09-16 16:28:30 +0530

    changing the payment

--- src/App.js
+++ src/App.js
@@ -28,18 +28,15 @@ import Auction from "./Pages/Auction/Auction";
 import CreateAuction from "./Pages/CreateAuction/CreateAuction";
 import AuctionDetail from "./Pages/AuctionDetails/AuctionDetail";
 import ScrollToTop from "./Components/ScrollToTop";
-import Shoping from "./Pages/Shoping/Shoping"
+import Shoping from "./Pages/Shoping/Shoping";
 import AuctionAddressPage from "./Pages/AuctionAddressPage/AuctionAddressPage";
 import AuctionOrderPage from "./Pages/AuctionOrderPage/AuctionOrderPage";
 import AuctionPlaceOrder from "./Pages/AuctionPlaceOrder/AuctionPlaceOrder";
 import AuctionPaymentMethod from "./Pages/AuctionPaymentMethod/AuctionPaymentMethod";
 import AuctionCartPage from "./Pages/AuctionCartPage/AuctionCartPage";
 
-
-
 // Set the base URL for all axios requests
-axios.defaults.baseURL =
-  process.env.REACT_APP_API_PROXY || "https://auctionwebapp-1.onrender.com";
+axios.defaults.baseURL = "https://auctionwebapp-1.onrender.com";
 
 function App() {
   return (
@@ -47,130 +44,180 @@ function App() {
       <ScrollToTop />
       <Header />
       <Routes>
-        <Route path="/" element={
+        <Route
+          path="/"
+          element={
             <>
               <Home />
               <Footer />
             </>
-          }/>
-        <Route path="/shoping" element={
+          }
+        />
+        <Route
+          path="/shoping"
+          element={
             <>
               <Shoping />
               <Footer />
             </>
-          }/>
-        <Route path="/products/:url" element={
+          }
+        />
+        <Route
+          path="/products/:url"
+          element={
             <>
               <ProductItems />
               <Footer />
             </>
-          }/>
-        <Route path="/cart" element={
+          }
+        />
+        <Route
+          path="/cart"
+          element={
             <>
               <CartPage />
               <Footer />
             </>
-          }/>
-        <Route path="/auctioncart" element={
+          }
+        />
+        <Route
+          path="/auctioncart"
+          element={
             <>
               <AuctionCartPage />
               <Footer />
             </>
-          }/>
-        <Route path="/signin" element={<LoginPage /> } />
-        <Route path="/signup" element={<RegisterPage />}/>
-        <Route path="/shipping" element={<AddressPage />}/>
-        <Route path="/auctionshipping" element={<AuctionAddressPage />}/>
-        <Route path="/payment" element={<PaymentMethod />}/>
-        <Route path="/auctionpayment" element={<AuctionPaymentMethod />}/>
-        <Route path="/placeorder" element={<PlaceOrder />}/>
-        <Route path="/auctionplaceorder" element={<AuctionPlaceOrder />}/>
-        <Route path="/order/:id" element={
+          }
+        />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="/shipping" element={<AddressPage />} />
+        <Route path="/auctionshipping" element={<AuctionAddressPage />} />
+        <Route path="/payment" element={<PaymentMethod />} />
+        <Route path="/auctionpayment" element={<AuctionPaymentMethod />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/auctionplaceorder" element={<AuctionPlaceOrder />} />
+        <Route
+          path="/order/:id"
+          element={
             <>
               <ProtectedRoute>
                 <OrderPage />
               </ProtectedRoute>
             </>
-          }/>
-        <Route path="/auctionorder/:id" element={<AuctionOrderPage />}/>
-        <Route path="/orderhistory" element={
+          }
+        />
+        <Route path="/auctionorder/:id" element={<AuctionOrderPage />} />
+        <Route
+          path="/orderhistory"
+          element={
             <>
               <ProtectedRoute>
                 <OrderHistory />
               </ProtectedRoute>
             </>
-          }/>
-        <Route path="/profile" element={
+          }
+        />
+        <Route
+          path="/profile"
+          element={
             <>
               <ProtectedRoute>
                 <ProfilePage />
               </ProtectedRoute>
             </>
-          }/>
-        <Route path="/search" element={<SearchPage />}/>
+          }
+        />
+        <Route path="/search" element={<SearchPage />} />
 
         {/* Admin Routes */}
-        <Route path="/admin/dashboard" element={
+        <Route
+          path="/admin/dashboard"
+          element={
             <>
               <AdminRoute>
                 <Dashboard />
               </AdminRoute>
             </>
-          }/>
-        <Route path="/admin/products" element={
+          }
+        />
+        <Route
+          path="/admin/products"
+          element={
             <>
               <AdminRoute>
                 <ProductListPage />
               </AdminRoute>
             </>
-          }  exact/>
+          }
+          exact
+        />
 
-        <Route path="/admin/product/:id" element={
+        <Route
+          path="/admin/product/:id"
+          element={
             <>
               <AdminRoute>
                 <ProductEditPage />
               </AdminRoute>
             </>
-          }/>
-        <Route path="/admin/orders" element={
+          }
+        />
+        <Route
+          path="/admin/orders"
+          element={
             <>
               <AdminRoute>
                 <OrderListPage />
               </AdminRoute>
             </>
-          } exact/>
-        <Route path="/admin/users" element={
+          }
+          exact
+        />
+        <Route
+          path="/admin/users"
+          element={
             <>
               <AdminRoute>
                 <UserListPage />
               </AdminRoute>
-            </>}/>
-        <Route path="/admin/user/:id" element={
+            </>
+          }
+        />
+        <Route
+          path="/admin/user/:id"
+          element={
             <>
               <AdminRoute>
                 <UserEditPage />
               </AdminRoute>
             </>
-          }/>
+          }
+        />
         {/* Seller Routes */}
-        <Route path="/seller/products" element={
+        <Route
+          path="/seller/products"
+          element={
             <>
               <SellerRoute>
                 <ProductListPage />
               </SellerRoute>
             </>
-          }/>
-        <Route path="/seller/orders" element={
+          }
+        />
+        <Route
+          path="/seller/orders"
+          element={
             <>
               <SellerRoute>
                 <OrderListPage />
               </SellerRoute>
             </>
-          }/>
-        <Route path="/auction" element={<Auction />}/>
-        <Route path="/create-auction" element={<CreateAuction />}/>
-        <Route path="/auctions/:id" element={<AuctionDetail />}/>
-
+          }
+        />
+        <Route path="/auction" element={<Auction />} />
+        <Route path="/create-auction" element={<CreateAuction />} />
+        <Route path="/auctions/:id" element={<AuctionDetail />} />
       </Routes>
     </Router>
   );
