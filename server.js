14e7e9449e83bfcea9d97136b8c86739dc2954e6
akshay <akshaytanwar1701@gmail.com>
2025-01-23 20:29:58 +0530

    solving cors

--- server.js
+++ server.js
@@ -23,17 +23,16 @@ dotenv.config();
 
 const app = express();
 
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
 // app.use(express.json(corsOptions));
 // app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.options('*', cors());
+app.use(cors(corsOptions));
 
 // implementing api v2/for paypal
 app.get("/api/v2/keys/paypal", (req, res) => {
