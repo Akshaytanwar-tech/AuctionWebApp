f2f830ca35cf285796a463233a1f17a6bed8e36a
akshay <akshaytanwar1701@gmail.com>
2025-01-23 19:49:04 +0530

    solving cors

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/api/v2/keys/paypal", (req, res) => {
 
 connectTomongo();
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
